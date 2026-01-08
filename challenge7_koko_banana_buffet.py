c5fa22358925c1a697f816386385c6420a975bc6
MuskanHaryah <muskanharyah36@gmail.com>
2026-01-08 13:55:12 +0500

    Optimized solution for challenge 7

--- challenge7_koko_banana_buffet.py
+++ challenge7_koko_banana_buffet.py
@@ -1,10 +1,35 @@
-def calculate_minimum_speed(piles, k):
-    max_pile = max(piles)
+# def calculate_minimum_speed(piles, k):
+#     max_pile = max(piles)
 
-    for s in range(1, max_pile + 1):
+#     for s in range(1, max_pile + 1):
+#         hours = 0
+#         for pile in piles:
+#             hours += (pile + s - 1) // s  # ceiling division
+
+#         if hours <= k:
+#             return s
+
+
+def calculate_minimum_speed(piles, k):
+    left, right = 1, max(piles)
+    answer = right
+
+    while left <= right:
+        mid = (left + right) // 2
         hours = 0
+
         for pile in piles:
-            hours += (pile + s - 1) // s  # ceiling division
+            hours += (pile + mid - 1) // mid
 
         if hours <= k:
-            return s
+            answer = mid
+            right = mid - 1
+        else:
+            left = mid + 1
+
+    return answer
+
+
+# Examples
+print(calculate_minimum_speed([5, 10, 3], 4))        # 5
+print(calculate_minimum_speed([5, 10, 15, 20], 7))  # 10
