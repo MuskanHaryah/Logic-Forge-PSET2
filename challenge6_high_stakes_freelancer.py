8137c385cec41e42f2c5c82070d76e8471615359
MuskanHaryah <muskanharyah36@gmail.com>
2026-01-08 13:51:00 +0500

    Optimized solution for challenge 6

--- challenge6_high_stakes_freelancer.py
+++ challenge6_high_stakes_freelancer.py
@@ -1,25 +1,58 @@
+# def maximize_freelance_profit(deadlines, profits):
+#     jobs = list(zip(deadlines, profits))
+#     n = len(jobs)
+#     max_profit = 0
+#     max_jobs = 0
+
+#     def backtrack(index, time, profit, count):
+#         nonlocal max_profit, max_jobs
+
+#         if index == n:
+#             if profit > max_profit:
+#                 max_profit = profit
+#                 max_jobs = count
+#             return
+
+#         # Skip job
+#         backtrack(index + 1, time, profit, count)
+
+#         # Take job if it fits
+#         if time + 1 <= jobs[index][0]:
+#             backtrack(index + 1, time + 1, profit + jobs[index][1], count + 1)
+
+#     backtrack(0, 0, 0, 0)
+#     return [max_jobs, max_profit]
+
+
 def maximize_freelance_profit(deadlines, profits):
-    jobs = list(zip(deadlines, profits))
-    n = len(jobs)
-    max_profit = 0
-    max_jobs = 0
+    jobs = sorted(zip(deadlines, profits), key=lambda x: x[1], reverse=True)
+    max_deadline = max(deadlines)
 
-    def backtrack(index, time, profit, count):
-        nonlocal max_profit, max_jobs
+    parent = list(range(max_deadline + 1))
 
-        if index == n:
-            if profit > max_profit:
-                max_profit = profit
-                max_jobs = count
-            return
+    def find(slot):
+        if parent[slot] != slot:
+            parent[slot] = find(parent[slot])
+        return parent[slot]
 
-        # Skip job
-        backtrack(index + 1, time, profit, count)
+    def union(u, v):
+        parent[v] = u
 
-        # Take job if it fits
-        if time + 1 <= jobs[index][0]:
-            backtrack(index + 1, time + 1, profit + jobs[index][1], count + 1)
+    total_profit = 0
+    jobs_done = 0
 
-    backtrack(0, 0, 0, 0)
-    return [max_jobs, max_profit]
+    for deadline, profit in jobs:
+        available_slot = find(deadline)
+        if available_slot > 0:
+            union(find(available_slot - 1), available_slot)
+            total_profit += profit
+            jobs_done += 1
 
+    return [jobs_done, total_profit]
+
+
+# Example
+print(maximize_freelance_profit(
+    deadlines=[2, 1, 2, 1, 3],
+    profits=[100, 19, 27, 25, 15]
+))
